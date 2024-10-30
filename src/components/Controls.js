d54db3997e67977623d592616226072f61535dfd
VRJGaming <48409358+VRJGaming@users.noreply.github.com>
2024-10-30 17:33:27 +0530

    Delete src directory

--- src/components/Controls.js
+++
@@ -1,9 +0,0 @@
-// src/components/Controls.js
-import React from 'react';
-import { OrbitControls } from '@react-three/drei';
-
-function Controls() {
-  return <OrbitControls enableRotate={false} enableZoom={false} />;
-}
-
-export default Controls;
